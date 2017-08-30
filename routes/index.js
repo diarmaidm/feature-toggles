21beb1a58310df6407a4a87335c6d05433e32d5f
diarmaidm <diarmaidm@users.noreply.github.com>
2017-08-30 21:34:44 +0100

    turn off feature flags

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let feature1 = false;
-  let feature2 = true;
+  let feature2 = false;
 
-  res.render('index', { title: 'Express', feature1: feature1, feature2: feature2 });
+  res.render('index', { title: 'Feature Toggles', feature1: feature1, feature2: feature2 });
 });
 
 module.exports = router;
